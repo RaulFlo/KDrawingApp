736c3dfda5c701016435d010779b52a56cf3cab7
Raul Flores <rflo.dev@gmail.com>
2023-04-11 22:10:08 -0500

    Add permissions request storage

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.drawingapp
 
+import android.app.AlertDialog
 import android.app.Dialog
 import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
@@ -8,13 +9,36 @@ import android.view.View
 import android.widget.ImageButton
 import android.widget.LinearLayout
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
+import android.Manifest
+import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+
+    val requestPermission: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
+            permissions ->
+            permissions.entries.forEach{
+                val permissionName = it.key
+                val isGranted = it.value
+
+                if(isGranted){
+                    Toast.makeText(this,"Permission Granted.",
+                    Toast.LENGTH_LONG).show()
+                }else{
+                    if(permissionName==Manifest.permission.READ_EXTERNAL_STORAGE){
+                        Toast.makeText(this,"Permission Denied.",
+                            Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -36,8 +60,14 @@ class MainActivity : AppCompatActivity() {
             showBrushSizeChooserDialog()
         }
 
+        val ibGallery : ImageButton = findViewById(R.id.ib_gallery)
+        ibGallery.setOnClickListener {
+            requestStoragePermission()
+        }
 
     }
+
+
 
     private fun showBrushSizeChooserDialog() {
         val brushDialog = Dialog(this)
@@ -81,6 +111,33 @@ class MainActivity : AppCompatActivity() {
             mImageButtonCurrentPaint = view
         }
 
+    }
+
+    private fun requestStoragePermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE,)
+        ){
+            showRationaleDialog("Kids Drawing App", "Kids Drawing App " +
+            "needs to Access Your External Storage")
+        }else{
+            requestPermission.launch(arrayOf(
+                Manifest.permission.READ_EXTERNAL_STORAGE
+                //TODO - ADD WRITING EXTERNAL STORAGE PERMISSION
+
+            ))
+        }
+
+    }
+
+    private fun showRationaleDialog(title: String, message: String){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel"){dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
     }
 
 
