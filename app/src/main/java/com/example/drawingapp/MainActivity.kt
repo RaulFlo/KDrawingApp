e8047b142247c0a5a06e2f2e9a0e1839d4b1354e
Raul Flores <rflo.dev@gmail.com>
2023-04-12 22:13:45 -0500

    add fun to image gallery

--- app/src/main/java/com/example/drawingapp/MainActivity.kt
+++ app/src/main/java/com/example/drawingapp/MainActivity.kt
@@ -14,12 +14,24 @@ import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import android.Manifest
+import android.content.Intent
+import android.provider.MediaStore
+import android.widget.ImageView
 import androidx.core.app.ActivityCompat
 
 class MainActivity : AppCompatActivity() {
 
     private var drawingView: DrawingView? = null
     private var mImageButtonCurrentPaint: ImageButton? = null
+
+    val openGalleryLauncher: ActivityResultLauncher<Intent> = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+        result ->
+        if(result. resultCode == RESULT_OK && result.data!= null){
+            val imageBackground: ImageView = findViewById(R.id.iv_background)
+
+            imageBackground.setImageURI(result.data?.data)
+        }
+    }
 
     val requestPermission: ActivityResultLauncher<Array<String>> =
         registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()){
@@ -31,6 +43,13 @@ class MainActivity : AppCompatActivity() {
                 if(isGranted){
                     Toast.makeText(this,"Permission Granted.",
                     Toast.LENGTH_LONG).show()
+
+                    val pickIntent = Intent(Intent.ACTION_PICK,
+                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+
+                    openGalleryLauncher.launch(pickIntent)
+
+
                 }else{
                     if(permissionName==Manifest.permission.READ_EXTERNAL_STORAGE){
                         Toast.makeText(this,"Permission Denied.",
